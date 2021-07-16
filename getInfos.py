da2e8d2898e5be2531adabc48f5d3d2e4cb3235d
0x4261756D <nebbenv.google@bs4a.de>
2021-07-16 20:32:29 +0200

    improved error handling primitive rate-limit handling

--- getInfos.py
+++ getInfos.py
@@ -4,6 +4,7 @@ import requests
 import sys
 import re
 import concurrent.futures
+import time
 
 if len(sys.argv) != 2 or sys.argv[1] in ['-h', '--help']:
 	print('''
@@ -23,14 +24,13 @@ def func(start, end):
 	infos = []
 	for ind in range(start, end):
 		print(f'{start}/{ind}/{end}')
-		#if line in seen:
-		#	print(seen[line])
-		#else:
 		s = text[ind].replace('\n', '').split('-')
 		i = s[0] + '-' + '0' * (3 - len(s[1])) + s[1]
 		i = i.replace('\n', '')
-		# print(i, end='|')
 		r = requests.get('http://www.cardmarket.com/en/YuGiOh/Products/Search?searchString=' + i)
+		while r.status_code == 429:
+			time.sleep(5)
+			r = requests.get('http://www.cardmarket.com/en/YuGiOh/Products/Search?searchString=' + i)
 		if 'Sorry, no matches for your query' in r.text or len(s) == 3:
 			r = requests.get('http://yugipedia.com/index.php?search=' + i.split('-')[0])
 			if 'can refer to ' in r.text:
@@ -50,21 +50,29 @@ def func(start, end):
 				versions = r.text.split(i)[-1].split('<td>')[3].split('</table>')[0].split('<br />')
 				version = re.sub(r'<.+?>', '',versions[int(s[2])-1]).replace(' ', '-')
 				version = '-V-' + s[2] + '-' + version
-			r.close()
 			r = requests.get('http://www.cardmarket.com/en/YuGiOh/Products/Singles/' + packname + '/' + cardname + version)
+			while r.status_code == 429:
+				time.sleep(5)
+				r = requests.get('http://www.cardmarket.com/en/YuGiOh/Products/Singles/' + packname + '/' + cardname + version)
 			if 'Invalid product!' in r.text and len(s) == 3:
-				r.close()
 				r = requests.get('http://www.cardmarket.com/en/YuGiOh/Products/Singles/' + packname + '/' + cardname + '-V-' + s[2])
-		name = r.text.split('<h1>')[1].split('<span')[0]
-		parts = r.text.split(' €')[1:5]
+				while r.status_code == 429:
+					time.sleep(5)
+					print('---Ratelimited---')
+					r = requests.get('http://www.cardmarket.com/en/YuGiOh/Products/Singles/' + packname + '/' + cardname + '-V-' + s[2])
+		tmp1 = r.text.split('<h1>')
+		tmp2 = r.text.split(' €')
+		if len(tmp1) < 2:
+			sys.exit(f'-----Got the wrong page: start: {start}, end: {end}, current: {ind}, {i}-----\n{r.text}, {r}')
+		if len(tmp2) < 6:
+			sys.exit(f'-----Too few prices: start: {start}, end: {end}, current: {ind}, {i}-----\n{r.text}, {r}')
+		name = tmp1[1].split('<span')[0]
+		parts = tmp2[1:5]
 		out = ''
 		for f in parts:
 			out += f.split('<span>')[1] + '|'
-		# print(out, end='')
 		out = i + '|' + out + name
 		infos.append(out)
-		# print(name)
-		# seen[line] = out
 	r.close()
 	return infos
 
